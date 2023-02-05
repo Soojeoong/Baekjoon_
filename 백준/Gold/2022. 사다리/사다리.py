5a7385112806523d4b35c61703f2d1645f50dbd3
Soojeoong <100748928+Soojeoong@users.noreply.github.com>
2023-02-05 20:15:11 +0900

    [Gold V] Title: 사다리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/2022. 사다리/사다리.py
@@ -0,0 +1,24 @@
+# https://www.acmicpc.net/problem/1722
+from sys import stdin
+
+def f(x, y, w):
+    h1 = (x**2 - w**2)**0.5
+    h2 = (y**2 - w**2)**0.5
+    c = (h1*h2)/(h1+h2)
+    return c
+
+x, y, c = map(float, stdin.readline().split())
+start, end = 1, min(x,y)
+result = 0
+
+while end-start >= 0.000001:
+    #이진 탐색
+    mid = (start+end)/2
+    if f(x, y, mid) >= c:
+        result = mid
+        start = mid
+    else:
+        end = mid
+# print(round(result, 3))
+# print(format(result, ".3f"))
+print(f'{result:.3f}')
